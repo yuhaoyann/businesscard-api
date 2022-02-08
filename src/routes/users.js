98820b652cb483c48469f81425666afcf31442b5
Fatuma <sfatex@gmail.com>
2022-02-08 10:31:49 -0500

    Update login and register routes with query that fetches all users from DB for authentication

--- src/routes/users.js
+++ src/routes/users.js
@@ -18,40 +18,41 @@ module.exports = (db) => {
       return res.status(400).send("Enter all fields");
     }
 
-    db.query(`SELECT * FROM users`)
-      .then(({ rows: users }) => {
-        // response.json(users);
-      
-    if (getUserByEmail(email, users)) {
-      return res.status(400).send("Email already exists");
-      // console.log("++",users)
-    }
+    db.query(`SELECT * FROM users`).then(({ rows: users }) => {
+      // response.json(users);
 
-    
-    const hashedPassword = bcrypt.hashSync(password, 10);
-    console.log(hashedPassword)
-    const queryString = `INSERT INTO users (first_name,last_name,email,hashedPassword) VALUES ($1,$2,$3,$4) returning *`;
-    const queryparams = [first_name, last_name, email, hashedPassword];
-    return db
-      .query(queryString, queryparams)
-      .then((result) =>
-        // return result.rows[0];
-        response.status(200).send("User successefully created")
-      )
-      .catch((error) => console.log(error));
-    })
+      if (getUserByEmail(email, users)) {
+        return res.status(400).send("Email already exists");
+        // console.log("++",users)
+      }
+
+      const hashedPassword = bcrypt.hashSync(password, 10);
+      console.log(hashedPassword);
+      const queryString = `INSERT INTO users (first_name,last_name,email,hashedPassword) VALUES ($1,$2,$3,$4) returning *`;
+      const queryparams = [first_name, last_name, email, hashedPassword];
+      return db
+        .query(queryString, queryparams)
+        .then((result) =>
+          // return result.rows[0];
+          response.status(200).send("User successefully created")
+        )
+        .catch((error) => console.log(error));
+    });
   });
 
   router.post("/login", (req, res) => {
     const { email, password } = req.body;
-    const user = authenticateUser(email, password, users);
-    if (!user) {
-      return res.status(403).send("User not found");
-    }
-    // req.session["user_id"] = user.id;
-    // res.redirect("");
-  });
+    db.query(`SELECT * FROM users`).then(({ rows: users }) => {
+      // response.json(users);
 
+      const user = authenticateUser(email, password, users);
+      if (!user) {
+        return res.status(403).send("User not found");
+      }
+      // req.session["user_id"] = user.id;
+      // res.redirect("");
+    });
+  });
   router.post("/logout", (req, res) => {
     req.session = null;
     res.redirect("");
