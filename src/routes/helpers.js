a968f76deb749fcd1d719473c0f8a9902a56c607
Fatuma <sfatex@gmail.com>
2022-02-08 01:20:51 -0500

    Add helper functions for checking email and password

--- src/routes/helpers.js
+++ src/routes/helpers.js
@@ -1,1 +1,26 @@
-const bcrypt = require('bcrypt');
+
+
+//checks for a user in users database with the  provided email and if found returns the user
+
+const getUserByEmail = function(email,users) {
+  for (const user in users) {
+    if (users[user].email === email) {
+      return users[user];
+    }
+  }
+  return null;
+};
+
+
+// authenticates and returns a valid  user
+
+const authenticateUser = function(email,password,users) {
+  const user = getUserByEmail(email,users);
+  if (user) {
+    if (bcrypt.compareSync(password, user.password)) {
+      return user;
+    }
+  }
+  return null;
+};
+module.exports =  { getUserByEmail,  authenticateUser };
