2aad38a41278a0f9950945a70a1166c064febcb7
Fatuma <sfatex@gmail.com>
2022-02-08 00:22:32 -0500

    Install and require bcrypt

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 module.exports = (db) => {
-  router.get('/users', (request, response) => {
+  router.get('/users', (req, res) => {
     db.query(`SELECT * FROM users`)
       .then(({ rows: users }) => {
         response.json(users);
@@ -9,5 +9,38 @@ module.exports = (db) => {
       .catch((error) => console.log(error));
   });
 
+  router.post("/register", (req, res) => {
+    const { email, password } = req.body;
+    if (email === "" || password === "") {
+      return res.status(400).send("Enter all fields");
+    }
+  
+    if (getUserByEmail(email, users)) {
+      return res.status(400).send("Email already exists");
+    }
+    const newUser = createNewUser(email,password);
+    users[newUser.id] = newUser;
+    req.session['user_id'] = newUser.id;
+    res.redirect("/urls");
+  });
+  
+
+
+  // router.get('/users', (request, response) => {
+  //   db.query(`SELECT * FROM users`)
+  //     .then(({ rows: users }) => {
+  //       response.json(users);
+  //     })
+  //     .catch((error) => console.log(error));
+  // });
+
+
+
+
+
+
+
   return router;
 };
+
+
