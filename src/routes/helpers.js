1421bab86d4186c015135d6474813a520207fe1b
Fatuma <sfatex@gmail.com>
2022-02-09 16:13:11 -0500

    Import jwt to file

--- src/routes/helpers.js
+++ src/routes/helpers.js
@@ -1,14 +1,16 @@
+const jwt = require("jsonwebtoken");
 const validateToken = function (req, res, next) {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
   if (token == null)
     return res.status(401).send({ status: "error", message: "No token" });
-  // console.log("token++++++", token);
+   console.log("token++++++", token);
   try {
     const dataStoredInToken = jwt.verify(token, process.env.JWT_SECRET);
     req.user = dataStoredInToken;
     next();
   } catch (error) {
+    console.log("\n autherrorr+++", error);
     return res
       .status(401)
       .send({ status: "error", message: "Invalid Session" });
