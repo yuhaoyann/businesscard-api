9f398d4096aefbe6f608edd676435d9189cb3fe9
Fatuma <sfatex@gmail.com>
2022-02-09 16:10:37 -0500

    Add user_id to the seeds

--- src/db/schema/development.sql
+++ src/db/schema/development.sql
@@ -1,21 +1,21 @@
 
-INSERT INTO users (first_name,last_name,email,password)
+INSERT INTO users (first_name, last_name, email, password)
 VALUES
-  ('Helen','Smith','helen@gmail.com','$2b$10$ZLcABB5gFMd5Wra511zDt.4vA4pERzDfcWI2kNB/kKovvTxya1ms'),
-  ('Welid','Semir','welid@gmail.com','$2b$10$ZLcABB5gFMd5Wra511zDt.4vA4pERzDfcWI2kNB/kKovvTxya1ms'),
-  ('Laila','Negash','laila@gmail.com','$2b$10$ZLcABB5gFMd5Wra511zDt.4vA4pERzDfcWI2kNB/kKovvTxya1ms');
+  ('Helen', 'Smith','helen@gmail.com', '$2b$10$ZLcABB5gFMd5Wra511zDt.4vA4pERzDfcWI2kNB/kKovvTxya1ms'),
+  ('Welid', 'Semir', 'welid@gmail.com', '$2b$10$ZLcABB5gFMd5Wra511zDt.4vA4pERzDfcWI2kNB/kKovvTxya1ms'),
+  ('Laila','Negash','laila@gmail.com', '$2b$10$iyqfu8WXZnjLVDMFgmfjpujgMLvSLXbDmBs5kTWVK1gtfcqD.P9gy');
   
-INSERT INTO cards (fullname,email,photo,title,company,phone,github,linkedin,facebook,instagram,bio)
+INSERT INTO cards (user_id, fullname, email, photo, title, company, phone, github, linkedin, facebook, instagram, bio)
 VALUES
- ('Helen Smith','helen@gmail.com',
+ (1,'Helen Smith','helen@gmail.com',
    'https://i.imgur.com/LpaY82x.png',
   'title1','company1','226-555-6789','github1','linkedin1','facebook1','instagram1',
   'bio'),
- ('Welid Semir','welid@gmail.com',
+ (2,'Welid Semir','welid@gmail.com',
    'https://i.imgur.com/T2WwVfS.png',
   'title2','company2','647-544-6892','github2','linkedin2','facebook2','instagram2',
   'bio'),
- ('Laila Negash','laila@gmail.com',
+ (3,'Laila Negash','laila@gmail.com',
   'https://i.imgur.com/3tVgsra.jpg',
   'title3','company3','226-634-8967','github2','linkedin2','facebook2','instagram2',
   'bio');
