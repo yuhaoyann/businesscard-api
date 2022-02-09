b1df7f0b606a55d0a8881939ae9d7acbe287e8bf
Fatuma <sfatex@gmail.com>
2022-02-08 20:42:15 -0500

    Romove test code that was written to check validateToken locally

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,5 +1,4 @@
 const router = require("express").Router();
-const { getUserByEmail, authenticateUser } = require("./helpers");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
@@ -10,9 +9,10 @@ module.exports = (db) => {
   router.get("/", (req, res) => {
     res.status(200).send({
       status: "ok",
-      message: "Welcome to ApI developed for digital business card",
-      about: "Business card app",
-      team: ["Fatuma", "Hayoan", "Stephen"],
+      message: "Welcome to API developed for digital business card",
+      about:
+        "Digital Business card app that allows sharing of cards between users ",
+      team: ["Fatuma", "Hayoan", "Stéphen"],
       today: new Date(),
     });
   });
@@ -33,15 +33,8 @@ module.exports = (db) => {
         .send({ status: "error", message: "Enter all fields" });
     }
 
-    console.log("++++++", req.body);
-
     db.query(`SELECT * FROM users WHERE email = $1`, [email]).then((result) => {
-      // response.json(users);
-
-      // if (getUserByEmail(email, users)) {
-      //   return res.status(400).send("Email already exists");
-      //   // console.log("++",users)
-      // }
+      
       if (result.rows.length > 0) {
         return res.status(400).send("Email already exists");
       }
@@ -53,7 +46,6 @@ module.exports = (db) => {
       return db
         .query(queryString, queryparams)
         .then((result) => {
-          // return result.rows[0];
           const user = result.rows[0];
           delete user.password;
           console.log("userrrrr", user);
@@ -76,41 +68,31 @@ module.exports = (db) => {
   router.post("/login", (req, res) => {
     const { email, password } = req.body;
     db.query(`SELECT * FROM users WHERE email = $1`, [email]).then((result) => {
-      // response.json(users);
-
-      // if (getUserByEmail(email, users)) {
-      //   return res.status(400).send("Email already exists");
-      //   // console.log("++",users)
-      // }
+      
       if (result.rows.length != 1) {
         return res.status(400).send("User not found");
       }
-      // response.json(users);
-      
-      if (bcrypt.compareSync(password, result.rows[0].password)) {
-        console.log("passss",result.rows[0].password)
-        res.status(200).send("Successful login")
-      }
     
-    return null;
-      
+      const user = result.rows[0];
+      if (bcrypt.compareSync(password, user.password)) {
+        // console.log("passss",result.rows[0].password)
+        // res.status(200).send("Successful login")
+        const token = jwt.sign(user, process.env.JWT_SECRET, {
+          expiresIn: "500h",
+        });
+        res.status(200).send({
+          status: "ok",
+          message: "Successefull Login",
+          user,
+          token,
+        });
+      }
+
+      return null;
     });
   });
 
-  router.get("/profile", (req, res) => {
-    const authHeader = req.headers["authorization"];
-    const token = authHeader && authHeader.split(" ")[1];
-    // console.log("token++++++", token);
-
-    console.log("\n authHeader+++", authHeader);
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    console.log(decoded); // bar
-    res.send({ token, decoded });
-  });
-
-  router.post("/logout", (req, res) => {
-    req.session = null;
-    res.redirect("");
-  });
+  
+  
   return router;
 };
