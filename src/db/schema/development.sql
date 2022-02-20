f386bd2174e813ec1590faf16c99cb71b33597a7
yuhaoyann <yuhaoyann@gmail.com>
2022-02-20 18:56:26 -0500

    update default img url

--- src/db/schema/development.sql
+++ src/db/schema/development.sql
@@ -8,15 +8,15 @@ VALUES
 INSERT INTO cards (user_id, fullname, email, photo, title, company, phone, github, linkedin, facebook, instagram, bio)
 VALUES
  (1,'Helen Smith','helen@gmail.com',
-   'https://i.imgur.com/LpaY82x.png',
+   'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgWWsl5twoOi5_XrlEZGJLqSsE9IlXctpXqg&usqp=CAU',
   'title1','company1','226-555-6789','github1','linkedin1','facebook1','instagram1',
   'bio'),
  (2,'Welid Semir','welid@gmail.com',
-   'https://i.imgur.com/T2WwVfS.png',
+   'https://images.unsplash.com/photo-1587402092301-725e37c70fd8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cHVwcHklMjBkb2d8ZW58MHx8MHx8&w=1000&q=80',
   'title2','company2','647-544-6892','github2','linkedin2','facebook2','instagram2',
   'bio'),
  (3,'Laila Negash','laila@gmail.com',
-  'https://i.imgur.com/3tVgsra.jpg',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWSOfVKWeUXO1igbOj5_JZ0ZehxDhk0DL2xw&usqp=CAU',
   'title3','company3','226-634-8967','github2','linkedin2','facebook2','instagram2',
   'bio');
 
